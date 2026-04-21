88ca97670bc19e6581d933bd2eb40b643187769e
yashita234 <kataruyashitareddy2605@gmail.com>
2026-04-21 10:58:08 +0530

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,9 @@
+temp = int(input("enter the temperature"))
+
+if temp > 38 :
+    print("summer temperature")
+
+elif temp < 15 :
+    print("winter temperature")
+elif temp >15 and temp >38 :
+    print("moderate temperature")
